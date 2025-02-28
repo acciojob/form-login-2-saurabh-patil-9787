e53517474242fa6340d109476e1dcc0cf708dafa
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-02-28 18:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let inputname = document.getElementById('fname')
+let inputlname = document.getElementById('lname')
+let inputPhone= document.getElementById('phone')
+let inputMail = document.getElementById('email')
+let inputBtn = document.querySelector('button')
+
+
+inputBtn.addEventListener('click',()=>{
+            alert(`First Name: ${inputname.value} last Name: ${inputlname.value} Phone Number:${inputPhone.value} Email ID:${inputMail.value}`)
+})
